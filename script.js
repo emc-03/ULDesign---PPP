b5bd77c7fde294b1627076ee7a4899f7f684cd79
Emily Carter <ecar123@wgu.edu>
2023-05-16 20:35:31 -0600

    add to github

---
+++ script.js
@@ -0,0 +1,8 @@
+function dropDownNav() {
+  var x = document.getElementById("myNavBar");
+  if (x.className === "navBar") {
+    x.className += "responsive";
+  } else {
+    x.className = "navBar";
+  }
+}
